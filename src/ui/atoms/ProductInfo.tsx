f8a794afe3d2931961fc03aa40090f86dc420a88
Piotr Kalman <pkalman@katalogmarzen.pl>
2024-02-09 18:33:21 +0100

    add header to product list item

--- src/ui/atoms/ProductInfo.tsx
+++ src/ui/atoms/ProductInfo.tsx
@@ -10,7 +10,7 @@ export function ProductInfo({ name, price, category }: ProductInfoProps) {
 	return (
 		<div className="p-4">
 			<p>
-				<p>{name}</p>
+				<h1 className="text-base font-bold">{name}</h1>
 				<p>{priceFormat(price / 100)}</p>
 			</p>
 			<p>category: {category}</p>
